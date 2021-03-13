29b4dcbc77bb075b1058ada37e0352c35ac82020
vtheskeleton <47392011+viluzi@users.noreply.github.com>
2021-03-13 19:42:43 +0200

    Final Edits

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const http = require("http");
 const fs = require("fs");
-const url = require("url");
-const port = 8080
+const socketio = require('socket.io');
+const port = 8005
 
-var sessions = {};
 
-http.createServer((req, res) => {
+var server = http.createServer((req, res) => {
 	if(req.method != "POST") {	
 		responseCode = 200;
 		content = fs.readFileSync("./index.html");
@@ -24,6 +23,7 @@ http.createServer((req, res) => {
 			req.on("end", () => {
                 let seperator = /\+/g
                 console.log(body.split("=")[1].replace(seperator, " "));
+				help(body.split("=")[1].replace(seperator, " "))
 				res.end("sendNotification h");
 			});
 		}
@@ -42,4 +42,16 @@ http.createServer((req, res) => {
 		}
 	}
 })
-	.listen(port);
+.listen(port);
+const io = socketio(server)
+io.on("connection", function (socket) {
+	console.log("Made socket connection");
+	socket.on("help", function (data) {
+		console.log(data)
+	  });
+});
+
+function help(data) {
+	io.emit("sendnotif", data)
+}
+  
